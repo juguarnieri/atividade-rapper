98f1178b313ad093ea938a7343700569d0d213df
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-10-10 11:18:25 -0300

    refactor: routes get and get by ID were fixed

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -45,35 +45,7 @@ let suspeitos = [
 suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).json(suspeitos);
   });
-  
-  // Rota para cadastrar um novo suspeito
-  suspeitosRoutes.post("/", (req, res) => {
-      const { nome, idade, descricao_fisica, envolvimento_atividade_suspeitas } = req.body;
-    
-  // Validação dos campos obrigatórios
-  if (!nome || !idade|| !envolvimento_atividade_suspeitas) {
-      return res.status(400).json({
-        message: "O campo nome, idade, envolvimento em atividades suspeitas é obrigatório!",
-      });
-    }
-    
-    // Validação de existência de envolvimento em atividades suspeitas
-    if (
-      envolvimento_atividade_suspeitas != "sim" &&
-      envolvimento_atividade_suspeitas != "não"
-    ) {
-      return res.status(400).send({
-        message: "Digite 'sim' ou 'não'!",
-      });
-    }
-    
-    // Validação para verificar se a idade é um número inteiro
-    if (Number.isInteger(idade) == false) {
-      return res.status(400).send({
-        message: "A idade do suspeito deve ser um número inteiro!",
-      });
-    }
-  });
+
   // Rota para buscar um suspeito pelo id
 suspeitosRoutes.get("/:id", (req, res) => {
     const { id } = req.params;
@@ -90,6 +62,5 @@ suspeitosRoutes.get("/:id", (req, res) => {
   
     return res.status(200).json(suspeito);
   });
-
 
 export default suspeitosRoutes;
