8f2299f9394f06039bb9476abad5f278b91439b0
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-10-10 11:47:23 -0300

    feat: add post method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -158,4 +158,51 @@ suspeitosRoutes.delete("/:id", (req, res) => {
     });
   });
 
+
+   // Rota para cadastrar um novo suspeito
+   suspeitosRoutes.post("/", (req, res) => {
+    const { nome, idade, descricao_fisica, envolvimento_atividade_suspeitas } = req.body;
+  
+// Validação dos campos obrigatórios
+if (!nome || !idade|| !envolvimento_atividade_suspeitas) {
+    return res.status(400).json({
+      message: "O campo nome, idade, envolvimento em atividades suspeitas é obrigatório!",
+    });
+  }
+  
+  // Validação de existência de envolvimento em atividades suspeitas
+  if (
+    envolvimento_atividade_suspeitas != "sim" &&
+    envolvimento_atividade_suspeitas != "não"
+  ) {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
+  
+  // Validação para verificar se a idade é um número inteiro
+  if (Number.isInteger(idade) == false) {
+    return res.status(400).send({
+      message: "A idade do suspeito deve ser um número inteiro!",
+    });
+  }
+
+    // Criação de um novo suspeito
+const novoSuspeito = {
+      id: Math.floor(Math.random() * 1000000),
+      nome,
+      idade,
+      descricao_fisica,
+      envolvimento_atividade_suspeitas,
+    }
+
+ // Adiciona o novo planeta ao array de planetas
+ suspeitos.push(novoSuspeito);
+
+return res.status(201).json({
+  message: "Suspeito cadastrado com sucesso!",
+  novoSuspeito,
+});
+});
+
 export default suspeitosRoutes;
