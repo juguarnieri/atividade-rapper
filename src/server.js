fb77994ba57cbe0ea167535554483b39079bdd63
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-10-10 08:31:04 -0300

    feat: create server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const serverPort = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello People!!!" });
+});
+
+app.listen(serverPort, () => {
+  console.log(`🎀 Server started on http://localhost:${serverPort}`);
+});
