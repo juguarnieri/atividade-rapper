11fda41f5e84d7a426f6babe1e796219b8dcf447
Júla Guarnieri <juliaguarnieri04@gmail.com>
2024-10-10 11:41:38 -0300

    feat: add delete route

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -9,35 +9,35 @@ let suspeitos = [
     nome: "Felipe Dev",
     idade: 35,
     descricao_fisica:[ "Cabelo curto e preto, moreno"],
-    envolvimento_atividade_suspeitas: false,
+    envolvimento_atividade_suspeitas: "sim",
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Luiza",
     idade: 16,
     descricao_fisica: ["Cabelo grande e preto, branca"],
-    envolvimento_atividade_suspeitas: true,
+    envolvimento_atividade_suspeitas: "não",
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Caio Lacerda",
     idade: 17,
     descricao_fisica: ["Cabelo enrolado, branco"],
-    envolvimento_atividade_suspeitas: true,
+    envolvimento_atividade_suspeitas: "sim",
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Larissa Siva",
     idade: 16,
     descricao_fisica: ["Cabelo enrolado, moreno"],
-    envolvimento_atividade_suspeitas: false,
+    envolvimento_atividade_suspeitas: "não",
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Júlia Guarnieri",
     idade: 16,
     descricao_fisica: ["Cabelo curto e castanho, branca"],
-    envolvimento_atividade_suspeitas: false,
+    envolvimento_atividade_suspeitas: "sim",
   },
 ];
 
@@ -61,6 +61,101 @@ suspeitosRoutes.get("/:id", (req, res) => {
     }
   
     return res.status(200).json(suspeito);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { nome, idade, descricao_fisica, envolvimento_atividade_suspeitas } = req.body;
+  
+    // Busca um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((suspect) => suspect.id == id);
+  
+     // Validação dos campos obrigatórios
+     if (!nome || !idade|| !envolvimento_atividade_suspeitas) {
+        return res.status(400).json({
+          message: "O campo nome, idade, envolvimento em atividades suspeitas é obrigatório!",
+        });
+      }
+
+        // Validação de existência de envolvimento em atividades suspeitas
+        if (
+          envolvimento_atividade_suspeitas != "sim" &&
+          envolvimento_atividade_suspeitas != "não"
+        ) {
+          return res.status(400).send({
+            message: "Digite 'sim' ou 'não'!",
+          });
+        }
+        
+        // Validação para verificar se a idade é um número inteiro
+        if (Number.isInteger(idade) == false) {
+          return res.status(400).send({
+            message: "A idade do suspeito deve ser um número inteiro!",
+          }); 
+        };
+  
+    suspeito.nome = nome;
+    suspeito.idade = idade;
+    suspeito.descricao_fisica = descricao_fisica;
+    suspeito.envolvimento_atividade_suspeitas = envolvimento_atividade_suspeitas;
+  
+    return res.status(200).json({
+      message: "Suspeito atualizado com sucesso!",
+      suspeito,
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Rota para deletar um suspeito
+suspeitosRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    // Busca um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((suspect) => suspect.id == id);
+  
+    // Verifica se o suspeito foi encontrado
+    if (!suspeito) {
+      return res
+        .status(404)
+        .json({ message: `Suspeito com id ${id} não encontrado!` });
+    }
+  
+    // Remove o suspeito do array de suspeitos
+    suspeitos = suspeitos.filter((suspect) => suspect.id != id);
+  
+    return res.status(200).json({
+      message: "Suspeito removido com sucesso!",
+      suspeito,
+    });
   });
 
 export default suspeitosRoutes;
